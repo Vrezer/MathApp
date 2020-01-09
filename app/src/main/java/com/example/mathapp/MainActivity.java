af3b8602036be23fd62d656ccee3dcc3055de1d7
Vrezer <44469941+Vrezer@users.noreply.github.com>
2020-01-09 15:55:30 +0100

    Paczatek pracy zrobiona tylko tabliczka mnozenia

---
+++ app/src/main/java/com/example/mathapp/MainActivity.java
@@ -0,0 +1,39 @@
+package com.example.mathapp;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+
+public class MainActivity extends AppCompatActivity {
+
+        @Override
+        protected void onCreate(Bundle savedInstanceState) {
+            super.onCreate(savedInstanceState);
+            setContentView(R.layout.activity_main);
+        }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.menu, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+            switch (item.getItemId())
+            {
+                case R.id.menuMultiplicationTable:
+                    startActivity(new Intent(MainActivity.this, MultiplicationTable.class));
+
+                    return true;
+                    default:
+            }
+        return true;
+    }
+}
