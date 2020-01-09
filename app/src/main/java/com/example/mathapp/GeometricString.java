5805655bd9231c06231462c04122b271b0fa3430
Vrezer <44469941+Vrezer@users.noreply.github.com>
2020-01-09 20:25:16 +0100

    Dodana zakładka Ciag geometryczny

---
+++ app/src/main/java/com/example/mathapp/GeometricString.java
@@ -0,0 +1,14 @@
+package com.example.mathapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class GeometricString extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_geometric_string);
+    }
+}
