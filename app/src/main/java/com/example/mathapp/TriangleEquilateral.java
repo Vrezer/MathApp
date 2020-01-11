e37cebc005a1605e9c30e151937df5332de96016
Vrezer <44469941+Vrezer@users.noreply.github.com>
2020-01-11 14:53:13 +0100

    Dodano nowa podzakładke do zakładki Trojkaty, Rownoboczny

---
+++ app/src/main/java/com/example/mathapp/TriangleEquilateral.java
@@ -0,0 +1,14 @@
+package com.example.mathapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class TriangleEquilateral extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_triangle_equilateral);
+    }
+}
